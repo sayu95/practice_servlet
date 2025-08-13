160c04fb1d60fa314423fbeab41cec487ab5c7ea
sayu95 <jeongsayu95@gmail.com>
2025-08-13 14:23:13 +0900

    get/post를 이용한 회원가입

--- testPjt/src/main/java/com/testPjt/TestServletClass.java
+++ testPjt/src/main/java/com/testPjt/TestServletClass.java
@@ -15,12 +15,10 @@ public class TestServletClass extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println(" -- doget() -- ");
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 	
